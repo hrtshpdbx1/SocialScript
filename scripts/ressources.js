8c916bffda221a8e8a64984599fa777c2b8126c2
Louise Moraldy <louisemoraldy@proton.me>
2025-12-04 11:52:07 +0100

    link ressources css

--- scripts/ressources.js
+++ scripts/ressources.js
@@ -8,7 +8,7 @@ const resources = [
         title: "Collectif Autiste de Belgique",
         labels: ["association", "communauté-autiste", "belgique"],
         description: "Collectif créé par et pour les personnes autistes adultes ou en questionnement en Belgique francophone. Le but est de répondre à diverses carences du système belge concernant la prise en charge de l'autisme chez l'adulte et de créer un espace d'entraide et de partage.",
-        image: "/img/ressources/collectifautismebelgique.png",
+        image: "img/ressources/collectifautismebelgique.png",
         links: [
             { text: "Site internet", url: "https://collectifautiste.be/" }
         ]
@@ -17,7 +17,7 @@ const resources = [
         title: "CLE autistes",
         labels: ["association", "communauté-autiste", "france"],
         description: "Le Collectif pour la Liberté d'Expression des Autistes défend les droits et la parole des personnes autistes en France. L'association milite pour une société plus inclusive et lutte contre les discriminations tout en promouvant l'auto-représentation des personnes autistes.",
-        image: "/img/ressources/cle.jpg",
+        image: "img/ressources/cle.jpg",
         links: [
             { text: "Site internet", url: "https://cle-autistes.fr/" }
         ]
@@ -26,7 +26,7 @@ const resources = [
         title: "Autistes Associé·es",
         labels: ["association", "groupe-de-parole", "communauté-autiste", "belgique"],
         description: "Association par et pour des personnes autistes, basée à Liège. Iels proposent diverses activités comme des permanences hebdomadaires, des ateliers thématiques ou des après-midis en espace libre pour se rencontrer et échanger dans un cadre bienveillant et accessible.",
-        image: "/img/ressources/autisteassocies.png",
+        image: "img/ressources/autisteassocies.png",
         links: [
             { text: "Site internet", url: "https://blogs.bawet.org/autistesassociees/" }
         ]
@@ -35,7 +35,7 @@ const resources = [
         title: "ESAES",
         labels: ["association", "étudiants", "communauté-autiste", "belgique"],
         description: "Association de fait qui a pour but de soutenir les étudiant·es autistes de l'enseignement supérieur francophone de Belgique. L'association rassemble aussi bien des personnes autistes que des personnes alliées et organise des rencontres, événements et actions de sensibilisation.",
-        image: "/img/ressources/logo-esaes.png",
+        image: "img/ressources/logo-esaes.png",
         links: [
             { text: "Serveur Discord", url: "https://discord.com/invite/w7pAnDcc3c" }
         ]
@@ -44,7 +44,7 @@ const resources = [
         title: "Discord du Collectif Autisme Belgique",
         labels: ["groupe-de-parole", "communauté-autiste", "en-ligne"],
         description: "Un espace numérique convivial pour échanger, poser des questions, partager des ressources et rencontrer d'autres personnes autistes ou en questionnement. Différents salons thématiques disponibles.",
-        image: "/img/ressources/discord.jpg",
+        image: "img/ressources/discord.jpg",
         links: [
             { text: "Serveur Discord", url: "http://discord.gg/4amTYxxwHs" }
         ]
@@ -53,7 +53,7 @@ const resources = [
         title: "LAVA",
         labels: ["association", "recherche", "sensibilisation", "belgique"],
         description: "Réseau belge de personnes autistes qui promeuvent leur propre production de savoirs sur l'autisme et le dialogue avec la recherche scientifique. LAVA organise des conférences, ateliers et événements de sensibilisation.",
-        image: "/img/ressources/laval_logo.avif",
+        image: "img/ressources/laval_logo.avif",
         links: [
             { text: "Site internet", url: "https://www.lavavzw.be/" }
         ]
@@ -62,7 +62,7 @@ const resources = [
         title: "Handikapables",
         labels: ["association", "handicap", "belgique"],
         description: "ASBL bruxelloise dont le but est d'offrir un lieu convivial et accessible pour les personnes en situation de handicap et de sensibiliser la société aux difficultés auxquelles elles peuvent faire face. L'association organise des événements culturels, des ateliers et des actions de terrain inclusives.",
-        image: "/img/ressources/handikapables.jpg",
+        image: "img/ressources/handikapables.jpg",
         links: [
             { text: "Page Facebook", url: "https://www.facebook.com/Handikapables/" }
         ]
@@ -71,7 +71,7 @@ const resources = [
         title: "F.R.i.D.A",
         labels: ["association", "féminisme", "intersectionnalité", "validisme"],
         description: "Collective Féministe Radicalement Inclusive et Définitivement Anti-validiste. Collective de femmes handicapées racisées qui milite pour l'intersectionnalité des luttes et la visibilité des personnes handicapées racisées. Propose des contenus militants, des ressources et du soutien communautaire.",
-        image: "/img/ressources/frida.jpg",
+        image: "img/ressources/frida.jpg",
         links: [
             { text: "Page Instagram", url: "https://www.instagram.com/collectivefrida/?hl=fr" }
         ]
@@ -80,7 +80,7 @@ const resources = [
         title: "Les Dévalideuses",
         labels: ["association", "féminisme", "handicap", "france"],
         description: "Association française handi-féministe créée par et pour les femmes et personnes non-binaires handicapées. Beaucoup de ressources informatives, pratiques et militantes à trouver sur leur site internet et leurs réseaux sociaux. Elles organisent également des événements et des actions de sensibilisation.",
-        image: "/img/ressources/devalideuses.jpg",
+        image: "img/ressources/devalideuses.jpg",
         links: [
             { text: "Site internet", url: "https://lesdevalideuses.org" }
         ]
@@ -91,7 +91,7 @@ const resources = [
         title: "Laziness Does Not Exist",
         labels: ["livre", "essai", "anglais", "neurodivergence"],
         description: "Essai en anglais qui déconstruit le mythe de la paresse et explore comment le capitalisme et les normes sociales imposent une productivité toxique. Particulièrement pertinent pour les personnes neurodivergentes qui subissent souvent des jugements sur leur fonctionnement différent.",
-        image: "/img/ressources/laziness9.jpg",
+        image: "img/ressources/laziness9.jpg",
         links: [
             { text: "Commander", url: "https://www.worldofbooks.com/en-gb/products/laziness-does-not-exist-book-devon-price-phd-9781982140113" }
         ]
@@ -100,7 +100,7 @@ const resources = [
         title: "Unmasking Autism",
         labels: ["livre", "essai", "anglais", "masking", "communauté-autiste"],
         description: "Livre en anglais écrit par un psychologue autiste qui explore le concept de 'masking' (masquage social) chez les personnes autistes. L'ouvrage offre des outils concrets pour accepter son autisme et vivre de manière plus authentique, tout en déconstruisant les stéréotypes.",
-        image: "/img/ressources/unmasking.webp",
+        image: "img/ressources/unmasking.webp",
         links: [
             { text: "Commander", url: "https://www.worldofbooks.com/en-gb/products/unmasking-autism-book-devon-price-9781800960541" }
         ]
@@ -109,7 +109,7 @@ const resources = [
         title: "La différence invisible",
         labels: ["livre", "BD", "témoignage", "diagnostic", "débutant"],
         description: "Roman graphique touchant qui raconte le parcours de Marguerite, une jeune femme autiste qui découvre tardivement son diagnostic. Une BD pédagogique et émouvante qui permet de mieux comprendre l'autisme au féminin et le quotidien des personnes concernées. Idéal pour découvrir le sujet.",
-        image: "/img/ressources/differenceinvisible.jpg",
+        image: "img/ressources/differenceinvisible.jpg",
         links: [
             { text: "Commander", url: "https://www.librel.be/livre/9782756072678-la-difference-invisible-julie-dachez-mademoiselle-caroline/" }
         ]
@@ -118,7 +118,7 @@ const resources = [
         title: "Comprendre l'autisme",
         labels: ["site-web", "vulgarisation", "sensibilisation", "débutant"],
         description: "Plateforme en ligne qui propose des articles de vulgarisation scientifique et des synthèses de recherches récentes sur l'autisme. Le site démystifie de nombreuses idées reçues et offre des informations fiables, accessibles et régulièrement mises à jour.",
-        image: "/img/ressources/LogoComprendreAutisme.png",
+        image: "img/ressources/LogoComprendreAutisme.png",
         links: [
             { text: "Site internet", url: "https://comprendrelautisme.com/" }
         ]
